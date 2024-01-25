6c07b61019493ad6b1232d2f007a5e241c5bcfc2
HAFIDYassine <yassine.hafid21@gmail.com>
2024-01-25 14:48:40 +0100

    Initial commit

---
+++ app/src/main/java/com/example/iabsence/CameraScreen.kt
@@ -0,0 +1,18 @@
+package com.example.iabsence
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun UseCamera() {
+    Button(onClick = { /*TODO*/ },
+        modifier = Modifier.fillMaxWidth(),
+        ) {
+        Text(text = "Use Camera")
+
+    }
+
+}
